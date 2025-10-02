f0d84a3489adc1a2176e29f81aa8714409aaf9bc
Yeh TzuChen <67541201+uniyeh@users.noreply.github.com>
2025-10-02 21:50:17 +0800

    Test2: YOLO11.yaml+coco+300epoches

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolo11-ae-single.yaml')  # build a new model from scratch
+model = YOLO('yolo11.yaml')  # build a new model from scratch
 
 # Use the model
 results = model.train(
     data='coco.yaml',
-    epochs=300
+    epochs=300,
+    name="origin"
 )  # train the model
 results = model.val()  # evaluate model performance on the validation set
 results = model('https://ultralytics.com/images/bus.jpg')  # predict on an image
